6de844e5d952db6206bfec5d012601eafabd3cfc
prakao <kpkrisrin@gmail.com>
2025-12-23 17:42:57 +0700

    Lab10_2_finished

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -8,10 +8,14 @@ int main (){
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
-	
+	string copytext;
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+	dest<<"-------------------- BOOM ---------------------\n";
+	while(getline(source,copytext)){
+		dest<<copytext<<"\n";
+	}
+	dest<<"-------------------- HA!! ---------------------";
     source.close();
     dest.close();
 	return 0;
